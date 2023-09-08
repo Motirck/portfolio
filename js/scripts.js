5534c12dd5b769501feaa892feaa1c496dd0b547
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2023-09-08 15:22:54 -0300

    Fix CSS and updated JS with new features of ES6

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
+    const sideNav = document.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#sideNav',
@@ -9,17 +8,14 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     }
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const responsiveNavItems = document.querySelectorAll('#navbarResponsive .nav-link');
+
+    for (let responsiveNavItem of responsiveNavItems) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
             }
         });
-    });
-
+    }
 });
